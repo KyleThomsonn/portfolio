a21b9d1c4aaf924839d60a8bea74745997f4725b
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-06-25 17:11:49 -0700

    added mouse effect and bg snowfall effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
 import './styles/styles.scss'
 import AppRouter from './routers/AppRouter'
+import AnimatedCursor from 'react-animated-cursor'
+import { Snowfall } from 'react-snowfall'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return(
-    <AppRouter />
+    <div>
+      {!isMobile && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: "#554EEF",
+            zIndex: 10000
+          }}
+          outerStyle={{
+            border: "3px solid #FA4B4C",
+            zIndex: 10000
+          }}
+        />
+      )}
+        <Snowfall
+            snowflakeCount={4}
+            radius={[100,120]}
+            wind={[-0.5, 2]}
+            speed={[0.5, 1]}
+          style={{
+            position: 'absolute',
+            width: '100vw',
+            height: '100vh',
+            zIndex: '-100',
+            filter: 'blur(30px)'
+          }}
+        />
+      <AppRouter />
+    </div>
   )
 }
 
